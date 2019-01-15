b4a79a562dee2aad71fe12896c5fa16c45eea668
e <luthernpu@gmail.com>
2019-01-15 22:40:08 +0800

    d

--- example/index.js
+++ example/index.js
@@ -1,29 +1,28 @@
-var LutherPromise = require('lutherPromise')
-const Promise = LutherPromise.deffered().promise
-console.log(Promise)
-new Promise((resolve, reject) => {
+var LutherPromise = require('lutherPromise').LutherPromise
+
+new LutherPromise((resolve, reject) => {
     setTimeout(() => {
       resolve(1)
     }, 2000)
-  }).then(result => {  //this should append to the new promise.
+  }).then(result => {  //this should append to the new LutherPromise.
       console.log('first top layer .then ', result)
       return result + 1
-    //note that the final .then is called once the prior promise chain actually completes.
+    //note that the final .then is called once the prior LutherPromise chain actually completes.
   }).then(result => {
     console.log('second top layer .then ', result)
-    //if we return a new promise here.... p will get set
-    return new Promise((resolve, reject) => {
+    //if we return a new LutherPromise here.... p will get set
+    return new LutherPromise((resolve, reject) => {
       resolve(result+1)
     })
   }).then(result => {
-    console.log('promise ', result)
-    return new Promise((resolve, reject) => {
+    console.log('LutherPromise ', result)
+    return new LutherPromise((resolve, reject) => {
       setTimeout(() => {
         console.log('resolving third top layer ' + result)
         resolve(result+1)
       }, 1000)
     }).then().then(t => {
-      console.log('inner promise ' + t)
+      console.log('inner LutherPromise ' + t)
       return t
     })
   }).then(result => {
